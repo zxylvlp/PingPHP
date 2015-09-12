7ca9def1080dfef33d2e2f494ff635815818ec06
zhaoxingyu <zhaoxingyu19920404@gmail.com>
2015-09-12 11:06:44 +0800

    test

--- src/main.py
+++ src/main.py
@@ -1,15 +1,15 @@
-from helper import *
-from monitor import *
-
-if __name__ == "__main__":
-    run()
-
 def run():
+    from .helper import initLogging,transFiles
     initLogging()
     transFiles()
 
 def see():
+
+    from .monitor import initMonitor
     run()
     initMonitor()
+
+if __name__ == "__main__":
+    run()
 
 
